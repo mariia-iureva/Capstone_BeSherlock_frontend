5a1c98ba1f630b6a4d12eae188cbac6a9546f63a
Masha <iiureva.mariia@gmail.com>
2023-01-31 10:29:02 -0800

    working condition with mui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import axios from "axios";
+import "./App.css";
+// import { makeStyles } from "@mui/styles";
+// import Button from "@mui/material/Button";
+// // // import { makeStyles } from "@material-ui/core/styles";
+// // // import Button from "@material-ui/core/Button";
+// import TextField from "@mui/material/TextField";
+// // // import TextField from "@material-ui/core/TextField";
+// import List from "@mui/material/List";
+// // // import List from "@material-ui/core/List";
+// import ListItemText from "@mui/material/ListItemText";
+// // import ListItemText from "@material-ui/core/ListItemText";
+// const useStyles = makeStyles((theme) => ({
+//   button: {
+//     margin: theme.spacing(3),
+//   },
+// }));
 function App() {
+  // const classes = useStyles();
+  const [values, setValues] = React.useState({
+    Message: "hello",
+    MessageList: [],
+  });
+  const getMessageList = () => {
+    let msgList = values.MessageList.map((msg) => {
+      return <ListItemText primary={msg} />;
+    });
+    return msgList;
+  };
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, [name]: event.target.value });
+  };
+  const handleClick = (event) => {
+    let url = "http://localhost:3001/postMessage";
+    let body = { message: values.Message };
+    var msgList = values.MessageList;
+    msgList.push("You: " + values.Message);
+    axios
+      .post(url, body)
+      .then((res) => {
+        msgList.push("Bot: " + res.data.bot_message);
+        setValues({ ...values, msgList });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    setValues({ ...values, msgList });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <List>{getMessageList()}</List>
+      <TextField
+        id="standard-name"
+        label="Message"
+        value={values.name}
+        onChange={handleChange("Message")}
+        margin="normal"
+      />
+      <Button
+        variant="contained"
+        // className={classes.button}
+        onClick={(e) => handleClick(e)}
+      >
+        Send
+      </Button>
     </div>
   );
 }
-
 export default App;
