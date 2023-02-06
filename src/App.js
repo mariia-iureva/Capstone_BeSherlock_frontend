7915deb92d885fc1d3fe774df61edf729b975e20
Masha <iiureva.mariia@gmail.com>
2023-02-06 15:12:45 -0800

    added env and updated gitignore

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import "./App.css";
 import MessageList from "./MessageList";
+// import ScrollToBottom from "react-scroll-to-bottom";
 
 function App() {
   const [inputMessage, setInputMessage] = useState("");
   const [messages, setMessages] = useState([]);
+  const [step, setStep] = useState(-1);
 
   const handleChange = (event) => {
     setInputMessage(event.target.value);
-    console.log("handleChange", event.target.value);
+    // console.log("handleChange", event.target.value);
+  };
+
+  // This thing is setting a timer to update input message after a pause
+  // instead of letter by letter
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      console.log("we're updating input after a pause:", inputMessage);
+    }, 500);
+    return () => clearTimeout(timer);
+  }, [inputMessage]);
+
+  // This thing triggers handleClick when key "Enter" is hit
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      console.log("input message after Enter:", inputMessage);
+      handleClick(inputMessage);
+    }
   };
 
   const handleClick = React.useCallback(() => {
@@ -17,8 +37,8 @@ function App() {
     if (!inputMessage) {
       return;
     }
-    let url = "http://localhost:3001/postMessage";
-    let body = { message: inputMessage };
+    let url = `${process.env.REACT_APP_BACKEND_URL}/postMessage`;
+    let body = { message: inputMessage, step: step };
     const msgList = messages;
     msgList.push("You: " + inputMessage);
     setMessages([...msgList]);
@@ -27,14 +47,19 @@ function App() {
       .then((res) => {
         msgList.push("Bot: " + res.data.bot_message);
         setMessages([...msgList]);
+        setStep(res.data.step);
         console.log("handleClick then", msgList);
       })
       .catch((err) => {
         console.log(err);
       });
     console.log("handleClick end", msgList);
+
+    // const chatContainer = chatContainerRef.current;
+    // chatContainer.scrollIntoView({ behavior: "smooth" });
+
     setInputMessage("");
-  }, [inputMessage, setInputMessage, messages, setMessages]);
+  }, [inputMessage, setInputMessage, messages, setMessages, step, setStep]);
 
   // const handleMessageSubmit = (event) => {
   //   event.preventDefault();
@@ -43,6 +68,14 @@ function App() {
   //   //   message: "",
   //   // });
   // };
+  // scrolling to bottom
+
+  const chatContainerRef = useRef(null);
+
+  useEffect(() => {
+    const chatContainer = chatContainerRef.current;
+    chatContainer.scrollIntoView({ behavior: "smooth" });
+  }, [chatContainerRef, messages]);
 
   return (
     <div className="App">
@@ -51,6 +84,7 @@ function App() {
         <div className="all_messages_bubble_outer_container">
           <div className="all_messages_bubble_inner_container">
             <MessageList messages={messages} />
+            <div className="TEST" ref={chatContainerRef}></div>
           </div>
         </div>
       </main>
@@ -62,6 +96,7 @@ function App() {
           placeholder="Write here"
           value={inputMessage}
           onChange={handleChange}
+          onKeyDown={handleKeyDown}
           margin="normal"
           type="text"
         />
--- src/MessageList.js
+++ src/MessageList.js
@@ -4,8 +4,7 @@ import Message from "./Message";
 import "./MessageList.css";
 
 const MessageList = ({ messages }) => {
-  // console.log("these are props", props);
-  console.log("these are messages", messages);
+  // console.log("these are messages", messages);
 
   return (
     <>
@@ -19,18 +18,5 @@ const MessageList = ({ messages }) => {
     </>
   );
 };
-
-// MessageList.propTypes = {
-//   messages: PropTypes.arrayOf(
-//     PropTypes.shape({
-//       id: PropTypes.number.isRequired,
-//       sender: PropTypes.string.isRequired,
-//       body: PropTypes.string.isRequired,
-//       timeStamp: PropTypes.string.isRequired,
-//     })
-// ).isRequired,
-// onUpdateMessage: PropTypes.func.isRequired,
-//   ),
-// };
 
 export default MessageList;
